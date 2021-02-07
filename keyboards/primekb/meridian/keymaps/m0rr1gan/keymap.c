eb64f621b0743abb8c56be6c9dae0a784bb98eb1
morrigan <m0rr1gan@posteo.co>
2021-02-06 21:18:45 -0500

    updated LEDs and added layer 2

--- keyboards/primekb/meridian/keymaps/m0rr1gan/keymap.c
+++ keyboards/primekb/meridian/keymaps/m0rr1gan/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
 		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          TG(1),
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, TG(2),
 		KC_LALT, KC_LSGN,                   KC_LGDL, KC_L1SP, KC_SPC,  KC_RGUI,                                     KC_RCTL, KC_LCAN, KC_RALT
     ),
     [1] = LAYOUT_split_bs_rshift(
@@ -41,5 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_CAPS, KC_END,  KC_PGDN, KC_VOLD, KC_VOLU, KC_MUTE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,                     _______,
                 _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______, _______,
                 _______, _______,                   _______, _______, _______, _______,                                     _______, _______, _______
+    ),
+    [2] = LAYOUT_split_bs_rshift(
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______,
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,                     _______,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______, _______,
+		_______, _______,                   _______, _______, _______,  _______,                                    _______, _______, _______
     )
 };
--- keyboards/primekb/meridian/meridian.c
+++ keyboards/primekb/meridian/meridian.c
@@ -37,10 +37,12 @@ bool led_update_kb(led_t led_state) {
     if (res) {
         writePin(B12, !led_state.caps_lock);  //Un-comment this line to enable in-switch capslock indicator
     if (led_state.caps_lock) {
-        rgblight_setrgb_at(0, 255, 0, 0); //green
+        rgblight_setrgb_at(255, 0, 0, 0);
+        //rgblight_setrgb_at(0, 255, 0, 0); //green
     } else {
         rgblight_setrgb_at(0, 0, 0, 0);
     }
+    /*
     if (led_state.num_lock) {
         rgblight_setrgb_at(0, 0, 255, 1); //blue
     } else {
@@ -51,18 +53,23 @@ bool led_update_kb(led_t led_state) {
     } else {
         rgblight_setrgb_at(0, 0, 0, 2);
     }
+    */
 }
     return res;
 }
 
 //Below is an exmaple of layer indication using one of the RGB indicatiors. As configured, uses the bottom indicator (2) to light up red when layer 1 is in use. 
-/*
 layer_state_t layer_state_set_kb(layer_state_t state) {
     if (get_highest_layer(state) == 1) {
-        rgblight_setrgb_at(255, 0, 0, 2);
+        rgblight_setrgb_at(0, 255, 0, 1);
+    } else {
+        rgblight_setrgb_at(0, 0, 0, 1);
+    }
+
+    if (get_highest_layer(state) == 2) {
+        rgblight_setrgb_at(0, 0, 255, 2);
     } else {
         rgblight_setrgb_at(0, 0, 0, 2);
     }
     return state;
 }
-*/
